f7aa7e2dae375d72357ab39f2faa6a45cda957d1
marcusfoppa123 <marcus@forsberg.biz>
2025-05-09 22:17:20 +0200

    ny logga igen

--- src/components/AnimatedLoadingScreen.tsx
+++ src/components/AnimatedLoadingScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import icon from "@/assets/@sportslinked-icon..png";
+import icon from "@/assets/sportslinked-icon.png";
 import nameLogo from "@/assets/sportslinked-logo.png";
 
 const useIsMobile = () => {
